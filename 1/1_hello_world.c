53d2dd8ffcded805adbf222d0c0d9921409c2e17
Igor_Mishurin <igor_mishurin@epam.com>
2017-09-12 15:05:50 +0200

    1.1. Hello world added. Added cli util to run compilation and execution.

---
+++ 1/1_hello_world.c
@@ -0,0 +1,10 @@
+/*
+* Excercise 1-1. Run the "hello, world" program on your system. 
+* Experiment with leaving out parts of the program, to see what error messages you get.
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    return 1;
+}
---
+++ runcc.py
@@ -0,0 +1,49 @@
+"""
+Command line tool which parses compilation options and passess it to the compiler.
+Then it runs execution of compiled code.
+"""
+
+import argparse
+import subprocess
+import glob
+import os
+
+PARSER = argparse.ArgumentParser(description='Arguments for compiler', usage='%(prog)s [options]')
+PARSER.add_argument('w', help='Show warnings')
+PARSER.add_argument('--comp', default='cc', help='Compiler executable')
+PARSER.add_argument('--sn', required=True, help='Solution number')
+PARSER.add_argument('--out', nargs='?', default=None, help='Destination')
+
+ARGS = PARSER.parse_args()
+
+OPTION_LIST = []
+
+# Add compiler
+OPTION_LIST.append(ARGS.comp)
+
+# Add warning mode
+if ARGS.w:
+    OPTION_LIST.append('-Wall')
+
+PATH = ARGS.sn.split('.')
+FILES = glob.glob(os.path.join(PATH[0], '{0}_**.c'.format(PATH[1])))
+
+# Add file to compile
+OPTION_LIST.append(FILES[0])
+
+# Add compiler output option
+OPTION_LIST.append('-o')
+if ARGS.out:
+    OUT = ARGS.out
+else:
+    OUT = os.path.splitext(FILES[0])[0]
+
+# Add output file
+OPTION_LIST.append(OUT)
+
+# Run compilation
+EXIT_CODE = subprocess.call(OPTION_LIST, stdout=os.sys.stdout)
+
+# Run execution
+if EXIT_CODE == 0:
+    subprocess.call([OUT], stdout=os.sys.stdout)
