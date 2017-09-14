567a3bbe0de4fbbdb48402dd671bbf32dcd2b604
Igor_Mishurin <igor_mishurin@epam.com>
2017-09-14 04:24:12 +0200

    1.7. Added EOF output solution.

---
+++ 1/7_eof_value_print.c
@@ -0,0 +1,11 @@
+/*
+* Excercise 1-7. Write program to print the value of EOF.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("EOF integer value is: %d\n", EOF);
+    printf("EOF character value is: %c\n", EOF);
+    return 0;
+}
