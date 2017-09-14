0f699444664e2717677191f3ffc1c20e53f9fdee
Igor_Mishurin <igor_mishurin@epam.com>
2017-09-14 04:15:04 +0200

    1.6. getchar comparison solution added.

--- 1/5_reversed_table.c
+++ 1/5_reversed_table.c
@@ -1,6 +1,6 @@
 /*
 * Excercise 1-5. Modify the temperature conversion program to print the table in reversed order,
-* that is, from 300 degrees to 0
+* that is, from 300 degrees to 0.
 */
 
 #include <stdio.h>
---
+++ 1/6_char_vs_eof.c
@@ -0,0 +1,11 @@
+/*
+* Excercise 1-6. Verify that the expression getchar() != EOF is 0 or 1.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Enter character(s) or hit CTRL^D\n");
+    printf("Result: %d\n", getchar() != EOF);
+    return 0;
+}
