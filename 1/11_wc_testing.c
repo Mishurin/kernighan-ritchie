9039a3a365bb141ddceeb0724d3879cf0924e86f
Igor_Mishurin <igor_mishurin@epam.com>
2017-09-22 18:04:42 +0200

    1.11. Added tests for wc program. Added support to pass arguments to executable.

---
+++ 1/11_wc_testing.c
@@ -0,0 +1,50 @@
+/*
+* Excercise 1-11. How would you test the word count program? 
+* What kinds of input are most likely to uncover bugs if there any
+*/
+
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+// 1. 
+// To run and test the program execute in the root: 
+// python .\runcc.py --comp gcc --sn 1.11 --opts "<" .\1\mocks\wc_valid_data.txt
+// "<" quoted to use it with window power shell
+// 2.
+// To compare the results with onses that can be retreived with "wc" run:
+// wc .\1\mocks\wc_valid_data.txt
+// Results should be similar
+// 3 32 162 (for c program)
+// 3  32 162 .\1\mocks\wc_valid_data.txt (for wc)
+// 3.
+// Testing empty file:
+// python .\runcc.py --comp gcc --sn 1.11 --opts "<" .\1\mocks\wc_empty.txt
+// 4.
+// Tests if executable file is also readable (tests iself in this case)w:
+// python .\runcc.py --comp gcc --sn 1.11 --opts "<" .\1\11_wc_testing.exe
+// Output: 195 1169 58952
+// 5. Run with wc:
+// wc .\1\11_wc_testing.exe
+// Output: 195  1704 58952 .\1\11_wc_testing.exe
+int main() {
+    
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+    return 0;
+}
--- runcc.py
+++ runcc.py
@@ -13,6 +13,7 @@ PARSER.add_argument('w', nargs='?', help='Show warnings')
 PARSER.add_argument('--comp', default='cc', help='Compiler executable')
 PARSER.add_argument('--sn', required=True, help='Solution number')
 PARSER.add_argument('--out', nargs='?', default=None, help='Destination')
+PARSER.add_argument('--opts', nargs='*', help='Parameters for executable')
 
 ARGS = PARSER.parse_args()
 
@@ -41,9 +42,17 @@ else:
 # Add output file
 OPTION_LIST.append(OUT)
 
+# Executable command line arguments
+EXEC_OPTIONS = []
+EXEC_OPTIONS.append(OUT)
+
+# Add options for compiled executable
+if ARGS.opts:
+    EXEC_OPTIONS.extend(ARGS.opts)
+
 # Run compilation
 EXIT_CODE = subprocess.call(OPTION_LIST, stdout=os.sys.stdout)
 
 # Run execution
 if EXIT_CODE == 0:
-    subprocess.call([OUT], stdout=os.sys.stdout)
+    subprocess.call(EXEC_OPTIONS, stdout=os.sys.stdout, shell=True)
