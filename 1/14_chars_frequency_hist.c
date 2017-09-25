8a04629792766f84f5c4d557525291a713ffeb68
Igor_Mishurin <igor_mishurin@epam.com>
2017-09-25 18:26:05 +0200

    1.13. Added solution for histogram of characters frequency.

---
+++ 1/14_chars_frequency_hist.c
@@ -0,0 +1,37 @@
+/*
+* Excercise 1-14. Write a program to print a histogram of the frequencies
+* of different characters in its input.
+*/
+
+#include <stdio.h>
+
+#define ASCII_MAX 255
+
+int main() {
+    
+    int registry[ASCII_MAX];
+    for (int i = 0; i <= ASCII_MAX; i++) {
+        registry[i] = 0;
+    }
+
+    char ch;
+
+    while ((ch = getchar()) != EOF) {
+        if(!(ch == '\n' || ch == '\r' || ch == '\t' || ch == ' ')) {
+            registry[ch] += 1;
+        }
+    }
+
+    for (int i = 0; i <= 255; i++) {
+        int k = registry[i];
+        if (k > 0) {
+            printf("%c:\n", i);
+            for (int j = 1; j <= k; j++) {
+                printf("|");
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
