fca298d00d81744d7a00eba20d1650db7b322096
Igor_Mishurin <igor_mishurin@epam.com>
2017-10-02 00:35:53 +0200

    1.15. Added temperature conversion function.

---
+++ 1/15_farhenheit_to_celsius_fn.c
@@ -0,0 +1,30 @@
+/*
+* Rewrite the temperature conversion program of Section 1.2 to use a
+* a function for conversion.
+*/
+
+#include <stdio.h>
+
+float from_fahr_to_celsius(float f);
+
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = from_fahr_to_celsius(fahr);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
+
+float from_fahr_to_celsius(float f) {
+    return (5.0/9.0) * (f - 32.0);
+}
