bc7753ba43660b1e842a4a29dc1a7aefd8ffae34
Igor_Mishurin <igor_mishurin@epam.com>
2017-10-19 12:00:43 +0200

    1.12 Added with samples.

---
+++ 1/21_entab.c
@@ -0,0 +1,59 @@
+/*
+* Exercise 1-21. Write a program entab that replaces strings of blanks by the minimum
+* number of tabs and blanks to achieve the same spacing. Use the same tab stops as
+* for detab. When either a tab or a single blank would suffice to reach a tab stop,
+* which should be given preference?
+*/
+
+// Run to test: 
+// python .\runcc.py --comp gcc --sn 1.21 --opts "<" .\1\mocks\spaced_indentation.txt ">" out.txt
+// python .\runcc.py --comp gcc --sn 1.21 --opts "<" .\1\mocks\spaced_program.py ">" tabbed_program.py
+
+#include <stdio.h>
+#define TABSIZE 4
+#define FRAME_MARGIN 2
+
+int get_frame(char s[], int lim);
+
+int main() {
+    int length;
+    char frame[TABSIZE + FRAME_MARGIN];
+
+    while ((length = get_frame(frame, TABSIZE)) > 0) {
+        printf("%s", frame);
+    }
+    return 0;
+}
+
+int get_frame(char s[], int lim) {
+    char c;
+    int i;
+    int trailing_space_count = 0;
+    int has_carriage_return = 0;
+    int has_new_line = 0;
+    for (i = 0; i < lim && (c = getchar()) != EOF; i++) {
+        if (c == ' ') {
+            trailing_space_count++;
+        } else if (c == '\n') {
+            has_new_line = 1;
+            s[i] = c;
+            i++;
+            break;
+        } else {
+            trailing_space_count = 0;
+        }
+        s[i] = c;  
+    }
+
+    if (trailing_space_count > 0) {
+        int tab_index = lim - trailing_space_count;
+        i = tab_index;
+        s[i] = '\t';
+        i++;
+    }
+
+    s[i] = '\0';
+
+    return i;
+}
+
---
+++ 1/mocks/spaced_program.py
@@ -0,0 +1,10 @@
+'''Dummy module'''
+
+def myfn():
+    '''Dummy function'''
+    if true:
+        return 1
+    else:
+        return 0
+
+print fn
