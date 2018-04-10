88ebddca197e2cce526ca62cf6c97e73939a98d6
Igor_Mishurin <igor_mishurin@epam.com>
2018-04-10 14:15:23 +0200

    2.2. Added alrernative line getter.

---
+++ 2/2_alt_line_reader.c
@@ -0,0 +1,45 @@
+/*
+* Exercise 2.2. Write a loop equivalent to the loop above without using && or ||.
+*/
+
+#include <stdio.h>
+
+#define MAX_LINE_LENGTH 1000
+
+int get_line(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1; i++)
+    {
+        if((c = getchar()) == EOF)
+        {
+            break;
+        } 
+        if(c == '\n')
+        {
+           break;
+        }
+        s[i] = c;
+    }
+    
+    if( c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
+
+int main()
+{
+    char line[MAX_LINE_LENGTH];
+    int length;
+    while((length = get_line(line, MAX_LINE_LENGTH)) > 0) 
+    {
+        printf("Line:%sLength:%d\n", line, length);
+    }
+    return 0;
+}
