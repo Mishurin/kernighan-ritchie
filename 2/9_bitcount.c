ede7ea74c12bd3a57259821ee08d0cdad6e3343a
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-05-07 19:46:05 +0200

    2.9. Added bitcount function

---
+++ 2/9_bitcount.c
@@ -0,0 +1,33 @@
+/*
+ * Exercise 2-9. In a two's complement number system, x &= (x-1) deletes the rightmost 
+ * 1-bit in x.
+ * Explain why. Use this observation to write a faster version of bitcount.
+*/
+
+#include <stdio.h>
+
+int convert(int a)
+{
+    if (a < 0)
+        a = ~(-a) + 1;
+    return a;
+}
+
+int bitcount(unsigned int x)
+{
+    int count;
+    for (count = 0; x != 0; x &= x - 1)
+    {
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    printf("Count:%d\n", convert(-1));
+    printf("Count:%d\n", bitcount(0));
+    printf("Count:%d\n", bitcount(5));
+    printf("Count:%d\n", bitcount(255));
+    return 0;
+}
