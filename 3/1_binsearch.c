759caa65d5a77636737e661212f5db70df4a40f8
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-05-12 15:54:50 +0200

    3.1. Reduced number of checks in the loop

--- 3/1_binsearch.c
+++ 3/1_binsearch.c
@@ -8,20 +8,22 @@
 
 int binsearch(int x, int v[], int n)
 {
-    int low, high, mid;
+    int low, high, mid = 0;
     low = 0;
     high = n - 1;
-    while (low <= high)
+    while (low <= high && x != v[mid])
     {
         mid = (low + high) / 2;
         if (x < v[mid])
             high = mid - 1;
-        else if (x > v[mid])
-            low = mid + 1;
         else
-            return mid;
+            low = mid + 1;
     }
-    return -1;
+
+    if (v[mid] == x)
+        return mid;
+    else
+        return -1;
 }
 
 int main()
