85c8cbbf0cc10315629b5cd3fa150c1ed3423211
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-05-12 14:31:43 +0200

    3.1 Added fixed binsearch function

---
+++ 3/1_binsearch.c
@@ -0,0 +1,34 @@
+/*
+* Exercise 3-1. Our binary search makes two tests inside the loop, when one would suffice 
+* (at the price of more tests outside.) Write a version with only one test inside the loop
+* and measure the difference in run-time.
+*/
+
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
+
+int main()
+{
+    int x = 5;
+    int v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int n = 10;
+    printf("Index: %d\n", binsearch(x, v, n));
+    return 0;
+}
