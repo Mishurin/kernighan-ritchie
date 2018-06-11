c38d2f8cb5eb349c568869aae1906eadf250bba5
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-06-11 20:29:24 +0200

    4.8. Added singlsingle character getch and ungetch

---
+++ 4/8_getch_ungetch.c
@@ -0,0 +1,45 @@
+/*
+* Exercise 4-8. Suppose that there will never be more than one character of pushback. 
+* Modify getch and ungetch accordingly.
+*/
+
+#include <stdio.h>
+#include <string.h>
+char buf;
+int bufset = 0;
+
+/* get a (possibly pushed-back) character */
+int getch(void)
+{
+    if(bufset) {
+        bufset = 0;
+        return buf; 
+    } else {
+        return getchar();
+    }
+}
+
+/* push character back on input */
+void ungetch(int c)
+{
+    buf = c;
+    bufset = 1;
+}
+
+int main()
+{
+    char c;
+    putchar(getch());
+    putchar(getch());
+    putchar(getch());
+    c = getch();
+    putchar(c);
+    ungetch(c);
+    putchar(getch());
+    c = getch();
+    if( c == '\n')
+    {
+        putchar(c);
+    }
+    return 0;
+}
