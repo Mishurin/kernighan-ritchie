2ced72940d68118b1e440d9c2fabc46aa7f5911f
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-06-16 12:33:08 +0200

    4.13. Added recursive reverse function.

---
+++ 4/13_reverse.c
@@ -0,0 +1,36 @@
+/*
+* Exercise 4-13. Write a recursive version of the function reverse(s), which reverses the 
+* string s in place.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* swap: interchange v[i] and v[j] */
+void swap(char v[], int i, int j)
+{
+    int temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+void reverse(char s[])
+{
+    static int i = 0;
+    int len = strlen(s);
+    int j = len - i - 1;
+    if (i < j)
+    {
+        swap(s, i++, j);
+        reverse(s);
+    }
+}
+
+int main()
+{
+    char str[] = "test";
+    reverse(str);
+    printf("%s\n", str);
+    return 0;
+}
