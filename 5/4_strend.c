a0a156d475d270f98e1d63e118208e73d52557b6
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-06-21 19:13:00 +0200

    5.4. Added strend function.

---
+++ 5/4_strend.c
@@ -0,0 +1,39 @@
+/*
+* Exercise 5-4. Write the function strend(s,t), which returns 1 if the string t occurs at 
+* the end of the string s, and zero otherwise.
+*/
+
+// Run test:
+// python ./runcc.py --comp gcc --sn 5.4
+
+#include <stdio.h>
+#include <string.h>
+
+int strend(char *, char *);
+
+int main()
+{
+    char s[] = "my test";
+    char t[] = "test";
+
+    printf("Result: %d\n", strend(s, t));
+    printf("Result: %d\n", strend(s, "test1"));
+    printf("Result: %d\n", strend(s, "test "));
+    printf("Result: %d\n", strend("test", "my test"));
+    printf("Result: %d\n", strend("my test", "my test"));
+    return 0;
+}
+
+int strend(char *s, char *t)
+{
+    int slen = strlen(s);
+    int tlen = strlen(t);
+    if(slen < tlen)
+        return 0;
+    while (--slen >= 0 && --tlen >= 0)
+    {
+        if ( *s + slen != *t + tlen)
+            return 0;
+    }
+    return 1;
+}
