a805f9973fae9227b47362e294cb8bcda65d6cea
igor_mishurin@epam.com <igor_mishurin@epam.com>
2018-06-27 20:41:14 +0200

    5.9. Added date utils with pointers.

--- 5/9_dates.c
+++ 5/9_dates.c
@@ -1,9 +1,10 @@
 /*
-* Exercise 5-8. There is no error checking in day_of_year or month_day. Remedy this defect.
+* Exercise 5-9. Rewrite the routines day_of_year and month_day with pointers instead of 
+* indexing.
 */
 
 // Run test:
-// python ./runcc.py --comp gcc --sn 5.8
+// python ./runcc.py --comp gcc --sn 5.9
 
 #include <stdio.h>
 
@@ -13,14 +14,12 @@ void month_day(int year, int yearday, int *pmonth, int *pday);
 int main()
 {
     printf("Result: %d\n", day_of_year(2018, 6, 26));
-    printf("Result: %d\n", day_of_year(-2018, 6, 26));
-    printf("Result: %d\n", day_of_year(2018, 0, 26));
-    printf("Result: %d\n", day_of_year(2018, 0, 32));
+    printf("Result: %d\n", day_of_year(2018, 6, 27));
     int month, day;
     month_day(2018, 177, &month, &day);
     printf("Result: month=%d, day=%d\n", month, day);
-    month_day(0, 177, &month, &day);
-    month_day(2015, 366, &month, &day);
+    month_day(2018, 178, &month, &day);
+    printf("Result: month=%d, day=%d\n", month, day);
     return 0;
 }
 
@@ -32,13 +31,21 @@ static char daytab[2][13] = {
 int day_of_year(int year, int month, int day)
 {
     int i, leap;
+    char *mpt, *cmpt;
+
     if (year <= 0 || month <= 0 || month > 12 || day <= 0 || day > 31)
     {
         return -1;
     }
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+    mpt = *(daytab + leap);
+    cmpt = mpt + month;
+    if (day > *cmpt)
+    {
+        return -1;
+    }
+    while (++mpt < cmpt)
+        day += *mpt;
     return day;
 }
 
@@ -46,6 +53,7 @@ int day_of_year(int year, int month, int day)
 void month_day(int year, int yearday, int *pmonth, int *pday)
 {
     int i, leap;
+    char *mpt;
     if (year <= 0 || yearday <= 0 || yearday > 366)
     {
         printf("Error: invalid input: year=%d, yearday=%d\n", year, yearday);
@@ -53,14 +61,15 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
     }
 
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    mpt = *(daytab + leap);
 
     if (!leap && yearday > 365)
     {
         printf("Non-leap year has only 365 days\n");
     }
 
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
+    for (i = 1; yearday > *(++mpt); i++)
+        yearday -= *mpt;
     *pmonth = i;
     *pday = yearday;
 }
